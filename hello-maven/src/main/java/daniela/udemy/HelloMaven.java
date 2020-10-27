c204671dd56e0efa7034959bed8c24c75059ca70
mixdanutza <bizgudaniela1@gmail.com>
2020-10-27 15:28:40 +0200

    Create static logger and simple info, debug logs

--- hello-maven/src/main/java/daniela/udemy/HelloMaven.java
+++ hello-maven/src/main/java/daniela/udemy/HelloMaven.java
@@ -1,7 +1,14 @@
 package daniela.udemy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        log.info("Hello Info");
+        log.debug("Hello Debug");
     }
 }
